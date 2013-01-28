bfc4f44717405daa064d0ead1dd9ebf6bd3a5130
John Smith <private@unknown.com>
2013-01-28 14:25:00 -0800

    added tests for model. they should all fail as I have not coded the features into user model

---
+++ protected/tests/unit/user_model.php
@@ -0,0 +1,91 @@
+<? 
+define('SAMPLE_USER_NAMES', serialize( array ('bob', 'joe', 'david', 'al', 'ronald') ) );
+define('SAMPLE_ROLES', serialize( array ('user', 'admin') ) );
+define('SAMPLE_PASSWORDS', serialize( array ('securepassword', 'password', 'pass', 'qwerty', 'i3=%~CT!', '*{S&LH2c','UFWmL3p','hCEjBVaqX','QJW58zts','4Vu5mm','XvNB85NDaB') ) );
+function get_random_element($const_array)
+{
+	$value_list = unserialize($const_array);
+	$index = rand(1, count($value_list)) - 1;
+	return $value_list[$index];
+}
+function generate_random_user_details()
+{
+	$password = get_random_element(SAMPLE_PASSWORDS);
+	$details = array(
+		'username' => get_random_element(SAMPLE_USER_NAMES),
+		'role' => get_random_element(SAMPLE_ROLES),
+		'password' => $password,
+	 	'password_repeat' => $password);
+	return $details;
+}
+
+function build_user_object($details)
+{
+	$user = new User;
+	if ($details['username'] 				!= null) 	{$user->username = 				$details['username'];}
+	if ($details['role'] 						!= null) 	{$user->role = 						$details['role'];}
+	if ($details['password']				!= null) 	{$user->password = 				$details['password'];}
+	if ($details['password_repeat'] != null) 	{$user->password_repeat = $details['password_repeat'];}
+	return $user;
+}
+
+function add_user_to_database($details)
+{
+	$user = build_user_object($details);
+	$user->save();
+	return $user;
+}
+
+class UserModelTest extends CTestCase
+{
+	protected function setUp()
+	{
+		User::model()->deleteAll();
+	}
+	protected function tearDown()
+	{
+		User::model()->deleteAll();
+	}
+	public function testPasswordConfirmationUsingReturnCode()
+	{
+		//This makes sure the User Model will not allow new users to be created unless their passwords match
+		$details = generate_random_user_details();
+
+		$details['password_repeat'] = $details['password'] + '123';
+		$user = build_user_object($details);
+		$result = $user->save();
+		$this->assertTrue($result != true);
+
+		$details['password_repeat'] = $details['password'];
+		$user = build_user_object($details);
+		$result = $user->save();
+		$this->assertTrue($result == true);
+	}
+	public function testPasswordConfirmationInDatabase()
+	{
+		//This makes sure the User Model will not allow new users to be created unless their passwords match
+		$details = generate_random_user_details();
+		$details['password_repeat'] = $details['password'] + '123';
+		$user = add_user_to_database($details);
+		$found_user = User::model()->findAllByAttributes(array('username'=> $details['username']));
+		$this->assertTrue(count($found_user) == 0);
+	}
+	public function testClearTextPasswordsInMemory()
+	{
+		//This checks to see if the model is keeping the users password as clear text in memory
+		$details = generate_random_user_details();
+		$user = add_user_to_database($details);
+		$this->assertTrue($user->password != $details['password']);
+	}
+	public function testClearTextPasswordsInDatabase()
+	{
+		//This checks to see if the model is keeping the users password as clear text in the database
+		$details = generate_random_user_details();
+		$user = add_user_to_database($details);
+		$found_user = User::model()->findAllByAttributes(array('username'=> $details['username']));
+		$this->assertTrue(count($found_user) != 0);
+		$this->assertTrue($found_user[0]['password'] != $details['password']);
+	}
+}
+?>
+
