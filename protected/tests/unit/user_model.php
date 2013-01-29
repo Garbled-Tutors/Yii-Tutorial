4bb5ae3289d35fdd03d2f9be0b0b5b27b1fdeaad
John Smith <private@unknown.com>
2013-01-28 16:38:56 -0800

    fixed errors in the user model tests

--- protected/tests/unit/user_model.php
+++ protected/tests/unit/user_model.php
@@ -44,19 +44,18 @@ class UserModelTest extends CTestCase
 	}
 	protected function tearDown()
 	{
-		User::model()->deleteAll();
+		//User::model()->deleteAll();
 	}
 	public function testPasswordConfirmationUsingReturnCode()
 	{
 		//This makes sure the User Model will not allow new users to be created unless their passwords match
 		$details = generate_random_user_details();
-
-		$details['password_repeat'] = $details['password'] + '123';
+		$details['password_repeat'] = $details['password'] . '123';
 		$user = build_user_object($details);
 		$result = $user->save();
 		$this->assertTrue($result != true);
 
-		$details['password_repeat'] = $details['password'];
+		$details = generate_random_user_details();
 		$user = build_user_object($details);
 		$result = $user->save();
 		$this->assertTrue($result == true);
@@ -65,7 +64,7 @@ class UserModelTest extends CTestCase
 	{
 		//This makes sure the User Model will not allow new users to be created unless their passwords match
 		$details = generate_random_user_details();
-		$details['password_repeat'] = $details['password'] + '123';
+		$details['password_repeat'] = $details['password'] . '123';
 		$user = add_user_to_database($details);
 		$found_user = User::model()->findAllByAttributes(array('username'=> $details['username']));
 		$this->assertTrue(count($found_user) == 0);
@@ -94,19 +93,23 @@ class UserModelTest extends CTestCase
 		$details = generate_random_user_details();
 		add_user_to_database($details);
 		$found_user = User::model()->findAllByAttributes(array('username'=> $details['username']));
-		$this->assertTrue(count($found_user) == 0);
-		$this->assertTrue($user['is_authenticated'] == false);
+		$this->assertTrue(count($found_user) == 1);
+		$this->assertTrue($found_user[0]['is_authenticated'] == false);
 	}
 	public function testPasswordAgainstPasswordHash()
 	{
 		$details = generate_random_user_details();
 		add_user_to_database($details);
+
 		$found_user = User::model()->findAllByAttributes(array('username'=> $details['username']));
-		$this->assertTrue(count($found_user) == 0);
-		$result = $found_user.authenticate($details['password'] + '123');
+		$this->assertTrue(count($found_user) == 1);
+
+		$user = $found_user[0];
+		$result = $user->authenticate($details['password'] . '123');
 		$this->assertTrue($result == false);
 		$this->assertTrue($user['is_authenticated'] == false);
-		$result = $found_user.authenticate($details['password']);
+
+		$result = $user->authenticate($details['password']);
 		$this->assertTrue($result == true);
 		$this->assertTrue($user['is_authenticated'] == true);
 	}
