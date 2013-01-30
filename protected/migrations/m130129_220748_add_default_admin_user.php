8ad17eb01994337d775888d24ef8c1f7ee8099f2
John Smith <private@unknown.com>
2013-01-30 14:14:57 -0800

    fixed an error in the migrations. The migration was crashing

--- protected/migrations/m130129_220748_add_default_admin_user.php
+++ protected/migrations/m130129_220748_add_default_admin_user.php
@@ -4,9 +4,7 @@ class m130129_220748_add_default_admin_user extends CDbMigration
 {
 	public function up()
 	{
-		$changeme_hash = <<<EOS
-$2a$08$Tt1n.BO/z/Pq8vDLlBzTC.Hcd/Iir20d1m.QoxARJMiaArZWA2es.
-EOS
+		$changeme_hash = '$2a$08$Tt1n.BO/z/Pq8vDLlBzTC.Hcd/Iir20d1m.QoxARJMiaArZWA2es.';
 		Yii::app()->db->createCommand("INSERT INTO `user` (`username`, `role`, `password`) VALUES ('admin', 'admin', '$changeme_hash');")->execute();
 	}
 
