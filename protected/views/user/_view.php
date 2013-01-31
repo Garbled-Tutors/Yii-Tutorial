7d588eb88a070ef5aa05908c09f7219db328ba05
John Smith <private@unknown.com>
2013-01-30 17:03:52 -0800

    Added an id field into user view so that acceptance testing will have an easier time navigating

--- protected/views/user/_view.php
+++ protected/views/user/_view.php
@@ -5,12 +5,8 @@
 
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
-
 	<b><?php echo CHtml::encode($data->getAttributeLabel('username')); ?>:</b>
-	<?php echo CHtml::encode($data->username); ?>
+	<?php echo CHtml::link(CHtml::encode($data->username), array('view', 'id'=>$data->id), array('id' => 'view-user-' . $data->username)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('role')); ?>:</b>
